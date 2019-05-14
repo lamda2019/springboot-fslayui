da39c3c151a86c3a23d0a1c973cde1f4b3eb8961
e <569188901@qq.com>
2019-05-14 17:46:41 +0800

    服务联调

---
+++ src/main/java/com/wsc/mapper/UserSqlProvider.java
@@ -0,0 +1,30 @@
+package com.wsc.mapper;
+
+import java.util.Map;
+
+/**
+ * 动态生成SQL.
+ * <p>
+ * 目标：使用Java工具类来替代传统的XML文件.(例如：UserSqlProvider.java <-- UserMapper.xml)
+ */
+public class UserSqlProvider {
+
+    //批量删除，坑，参数需要@Param，否则取不到
+    public String deletePatchUserInfo(Map map) {
+        StringBuilder sb = new StringBuilder();
+        int[] id= (int[]) map.get("id");
+        sb.append("DELETE FROM t_user WHERE id IN (");
+        for (int i = 0; i < id.length; i++) {
+            sb.append(id[i]+",");
+        }
+        if ( id.length > 0 ){
+            sb.deleteCharAt(sb.lastIndexOf(","));
+        }
+        sb.append(")");
+        return sb.toString();
+
+    }
+
+
+
+}
